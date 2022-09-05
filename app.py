8741f9af1925e05ab5e2598a3b4749bfbe90fde2
Fabio Ferreira da Silva <13707516+fabiocore@users.noreply.github.com>
2022-09-04 23:18:37 -0300

    ReallyNotBad app

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['POST', 'GET'])
+def notbad():
+    if request.method == 'POST' and bool(request.headers.get("NotBad")):
+            message = "ReallyNotBad"
+            return message
+    else:
+        return "<h1>hmmm... try setting a magical header.</h1>"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5080))
+    app.run(debug=True, host='0.0.0.0', port=port)
